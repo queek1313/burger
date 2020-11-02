7ba00fb3fe1442a4db343132cf99fa525465caff
tony nolan <tonynolan313@gmail.com>
2020-11-02 14:30:09 -0700

    heroku details

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
 const mysql = require("mysql")
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "chefness",
-    database: "burgers_db"
-});
+
+var connection;
+
+if (process.env.JAWSDB_RUL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "chefness",
+        database: "burgers_db"
+
+    });
+};
 connection.connect(function (error) {
     if (error) throw error;
     console.log("connected as id " + connection.threadId);
