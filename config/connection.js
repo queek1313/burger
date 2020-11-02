703af78b708e9ff052e726ac07c3ecc5dba610ab
tony nolan <tonynolan313@gmail.com>
2020-11-02 14:53:07 -0700

    heroku added

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql")
 
 var connection;
 
-if (process.env.JAWSDB_RUL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
